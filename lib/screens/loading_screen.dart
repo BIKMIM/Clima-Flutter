dfbad767adf787b2a1af81697d7944bb2b5db4f9
BIKMIM <2cp4us@gmail.com>
2020-08-19 22:23:51 +0900

    lesson 163 done.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,12 +1,9 @@
 import 'package:clima/screens/location_screen.dart';
-import 'package:clima/services/location.dart';
-import 'package:clima/services/networking.dart';
+import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import 'location_screen.dart';
-
-const apiKey = 'd9bbd44958d1ff6b2dbe3cd34ea45bb8';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -17,19 +14,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    Location();
-    print('this line of cord is triggered.');
     getLocationData();
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-
-    NetworkHelper networkHelper = NetworkHelper(
-        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
-
-    var weatherData = await networkHelper.getData();
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(
       context,
